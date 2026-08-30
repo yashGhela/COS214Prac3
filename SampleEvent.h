90397e862adb195b5a3e22c7007c29f7f17cded5
JayDawg439 <jaydenwaltersmith@gmail.com>
2026-08-30 18:26:15 +0200

    Task 2 Completed

---
+++ SampleEvent.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "EventComponent.h"
+#include "Venue.h"
+#include "Zone.h"
+#include "ProgrammeArea.h"
+#include "Bathroom.h"
+#include "Stage.h"
+#include "FoodTruck.h"
+#include "Screen.h"
+#include "PopCorn.h"
+#include "Medical.h"
+#include "Tent.h"
+#include <iostream>
+
+void runEvent();
